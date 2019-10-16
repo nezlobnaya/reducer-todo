91dabbf3acf0552e03a1d4076b0b7901fa1c41d5
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-16 13:47:12 -0400

    Built a simple reducer & initial state object

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+      }
+];
+
+
+export function todoReducer(state, action) {
+    return state;
+}
